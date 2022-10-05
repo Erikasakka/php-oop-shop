023b18cc114fcfd11a57df7444ef0f69f3037efd
Erikasakka <erikasakka29@gmail.com>
2022-10-05 19:52:38 +0300

    naujas

--- classes/Product.php
+++ classes/Product.php
@@ -5,19 +5,32 @@ require_once dirname(__FILE__) . '/../support/FileReader.php';
 class Product
 {
     // TODO: add missing parameters
-
+    public $id;
+    public $maker;
+    public $images;
+    public $url;
     public $title;
+    public $description;
+    public $price;
+    public $ratings;
 
-    public function __construct($title)
+    public function __construct(int $id, mixed $maker, array $images, mixed $url, string $title, mixed $description, float $price, mixed $ratings)
     {
-        // TODO: finish __construct() method
+        //TODO: finish __construct()
+        $this->id = $id;
+        $this->maker = $maker;
+        $this->images = $images;
+        $this->url = $url;
         $this->title = $title;
+        $this->description = $description;
+        $this->price = $price;
+        $this->ratings = $ratings;
     }
 
     public static function find($id)
     {
         // How to load data:
-        // $content = Product::getProducts('./data/products.json');
+        $content = Product::getProducts('./data/products.json');
 
         // TODO: check if given product exists, if exists return as object else return false
     }
--- products.php
+++ products.php
@@ -1,18 +1,59 @@
+<html>
+
+<head>
+  <title>
+    Products list
+  </title>
+</head>
+
+
 <?php
 
-// require_once('./classes/Product.php');
-
-// TODO: add products list page
-// $products = Product::getProducts('./data/products.json');
+//require_once('./classes/Product.php');
+//TODO: add products list page
+$products = Product::getProducts('./data/products.json');
+require_once('./classes/Product.php');
+$products = [];
+for ($i = 0; $i < 5; $i++) {
+  $images = [
+    'https://picsum.photos/200',
+    'https://picsum.photos/200'
+  ];
+  $products[] = new Product(
+    id;
+    maker;
+    images;
+    url;
+    title;
+    description;
+    price;
+    ratings;
+    title: 'product_name_' . $i,
+    price: rand(1, 100),
+    images: $images
+  );
+}
 ?>
 
-<html>
-  <head>
-    <title>
-      Products list
-    </title>
-  </head>
-  
-  <body>
-  </body>
+
+<div>
+  <?php foreach ($products as $product) : ?>
+    <div>
+      <h3>
+        <?php echo $product->title; ?>
+      </h3>
+      <span>
+        <?php echo $product->price; ?> Eur
+      </span>
+      <div>
+        <?php foreach ($product->images as $image) : ?>
+          <img src="<?php echo $image ?>">
+        <?php endforeach; ?>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
+
+<body>
+
 </html>
